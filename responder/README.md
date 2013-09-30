6fc9951615fec60819037c907ad2f73f05b80b58
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-09-30 00:18:18 -0700

    First pass at Responder

---
+++ responder/README.md
@@ -0,0 +1,3 @@
+# Responder
+
+Sets up an away message: will comment with a custom message on every new pull request and issue.
---
+++ responder/responder.rb
@@ -0,0 +1,16 @@
+require 'captain_hook/app'
+
+class Responder < CaptainHook::App
+  def on_pull_request(owner, repo_name, action, number, pull_request_json)
+    comment_like_a_parrot(owner, repo_name, number) if action == 'opened'
+  end
+
+  def secret(owner, repo_name, event_type)
+    ENV['RESPONDER_SECRET']
+  end
+
+  def comment_like_a_parrot(owner, repo_name, number)
+    message = 'Squawk! Thank you for the pull request. Squawk!'
+    CaptainHook::Commenter.new(ENV['RESPONDER_TOKEN']).comment_on_issue(owner, repo_name, number, message)
+  end
+end
