c79bb77c4a2fe9e69cf22afdc7e75104e27db2cc
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-09-30 15:56:44 -0700

    Responder should handle issues too

--- responder/responder.rb
+++ responder/responder.rb
@@ -5,12 +5,16 @@ class Responder < RightHook::App
     comment_like_a_parrot(owner, repo_name, number) if action == 'opened'
   end
 
+  def on_issue(owner, repo_name, action, issue_json)
+    comment_like_a_parrot(owner, repo_name, issue_json['number']) if action == 'opened' || action == 'reopened'
+  end
+
   def secret(owner, repo_name, event_type)
     ENV['RESPONDER_SECRET']
   end
 
   def comment_like_a_parrot(owner, repo_name, number)
-    message = 'Squawk! Thank you for the pull request. Squawk!'
+    message = 'Squawk! Thank you for the pull request or issue. Squawk!'
     RightHook::Commenter.new(ENV['RESPONDER_TOKEN']).comment_on_issue(owner, repo_name, number, message)
   end
 end
